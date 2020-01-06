39caa465927a507a5f1147192a350cc24bb8af0b
Benjamin <benjamin.adida@hetic.net>
2020-01-06 23:23:38 +0100

    navbar templating

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,8 @@
 require('../css/app.css');
 require('../scss/app.scss');
 const $ = require('jquery');
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
 require('bootstrap');

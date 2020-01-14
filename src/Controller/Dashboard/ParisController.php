ad8639877ff6c978cb029cbdd0ffbcc5977c1ad4
Benjamin <benjamin.adida@hetic.net>
2020-01-14 13:53:37 +0100

    edit method of the API

--- src/Controller/Dashboard/ParisController.php
+++ src/Controller/Dashboard/ParisController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Dashboard;
 
 use App\Entity\Paris;
 use App\Form\ParisType;
+use App\Repository\ParisRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -42,23 +43,6 @@ class ParisController extends AbstractController
         return $this->render('paris/index.html.twig', compact('paris'));
     }
 
-    /**
-     * @Route("/paris/{id}/delete", name="delete-paris")
-     * @param $id
-     * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse
-     * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
-     */
-    public function deleteParis($id, Request $request)
-    {
-        $client = HttpClient::create();
-
-        $response = $client->request('DELETE', 'http://dataviz-api.benjaminadida.fr/api/paris/'.$id.'/delete', [
-            'auth_bearer' => $this->getApiToken(),
-        ]);
-
-        return $this->redirectToRoute('index-paris');
-    }
 
     /**
      * @Route("/paris/create", name="create-paris")
@@ -92,5 +76,69 @@ class ParisController extends AbstractController
         return $this->render('paris/create.html.twig', [
             'form' => $form->createView()
         ]);
+    }
+
+    /**
+     * @Route("/paris/{id}/edit", name="update-paris")
+     * @param $id
+     * @param Request $request
+     * @param ParisRepository $repository
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
+     */
+    public function editParis($id, Request $request, ParisRepository $repository)
+    {
+        $client = HttpClient::create();
+
+        $response = $client->request('GET', 'http://dataviz-api.benjaminadida.fr/api/paris/' . $id, [
+            'auth_bearer' => $this->getApiToken(),
+        ]);
+        $paris = $response->toArray();
+
+        $submittedToken = $request->request->get('_csrf_token');
+
+        $url = 'https://dataviz-api.benjaminadida.fr/api/paris/' . $id . '/edit';
+
+        // 'delete-item' is the same value used in the template to generate the token
+        if ($this->isCsrfTokenValid('create-item', $submittedToken)) {
+            $client->request('PUT', $url, [
+                'json' => [
+                    'borough' => $request->request->get('borough'),
+                    'district' => $request->request->get('district'),
+                    'count_hotel' => $request->request->get('count_hotel'),
+                    'latitude' => $request->request->get('latitude'),
+                    'longitude' => $request->request->get('longitude')
+                ],
+                'auth_bearer' => $this->getApiToken(),
+            ]);
+            $this->addFlash('success', 'L\'item a été modifié avec succès.');
+            return $this->redirectToRoute('index-paris');
+        }
+
+        return $this->render('paris/edit.html.twig',[
+            'paris' => $paris
+        ]);
+    }
+
+    /**
+     * @Route("/paris/{id}/delete", name="delete-paris")
+     * @param $id
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
+     */
+    public function deleteParis($id, Request $request)
+    {
+        $client = HttpClient::create();
+
+        $response = $client->request('DELETE', 'http://dataviz-api.benjaminadida.fr/api/paris/'.$id.'/delete', [
+            'auth_bearer' => $this->getApiToken(),
+        ]);
+
+        return $this->redirectToRoute('index-paris');
     }
 }
